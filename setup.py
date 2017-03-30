bdcea26cf2a8badac27568b2b9d0ff415d9efcbb
Kevin Short <kevin.short@rackspace.com>
2017-03-30 16:15:19 -0400

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ AUTHOR = 'Kevin Short'
 AUTHOR_EMAIL = 'kevinshortnh@gmail.com'
 URL = 'http://www.oxmicro.com/'
 
-VERSION = '1.1.0'
+VERSION = '1.2.0'
 
 SCRIPT = '{}={}.{}:{}'.format(NAME, PACKAGE, 'hello', 'main')
 
